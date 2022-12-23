b0a738e3835a7f165e3486b644ea6889140aa59a
Ben Agro <bagro@waabi.ai>
2022-12-22 20:33:51 -0500

    basic scraping done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # moonmaker
+
+## Scraping Dependancies
+
+- tesseract
+- scipy
+- numpy
+- cv2
+- android studio or other android emulator
+- python adb
--- scrape_data.py
+++ scrape_data.py
@@ -1,11 +1,13 @@
 
 from ppadb.client import Client
 from typing import Tuple, List
+import json
 import io
 import cv2
 import numpy as np
 import imageio
 from scipy.ndimage.filters import maximum_filter
+import pytesseract
 
 # user: moonscraper
 # pwd: moonscraper 
@@ -16,14 +18,12 @@ TEMPLATE_H, TEMPLATE_W = CIRCLE_TEMPLATE.shape
 FINISH_COLOR = np.array([255, 0, 0, 255])
 START_COLOR = np.array([0, 255, 0, 255])
 MIDDLE_COLOR = np.array([0, 0, 255, 255])
-#COORDS:
+#Rough bolthole coordinates:
 #       A         B          ...   K
 # 18   (156, 511) (238, 511)
 # 17   (156, 594)  ...
 # ...
 # 1                                (983, 1919)
-
-
 
 def connect_to_device():
     adb = Client(host='127.0.0.1', port=5037)
@@ -103,27 +103,86 @@ def get_codes_for_color(img: np.array, color: np.array) -> List[Tuple[str, int]]
         pts_set.add(code)
     return sorted(pts_list, key = lambda x: (x[1], x[0]))
 
+def get_basic_climb_info(img: np.array) -> Tuple[str, str, str]:
+    """Given a climb screenshot, extract
+    climb name, grade, and setter
+
+    Args:
+        img (np.array): Climb screenshot
+    Returns:
+        name, grade, setter
+    """
+    info = pytesseract.image_to_string(img[129:365, 106:980])
+    lines = info.split("\n")
+    name, grade = lines[0].split(",")
+    grade = grade.strip()
+    setter = lines[1].split(":")[1].strip()
+    return name, grade, setter
+
+def code_to_string(code: Tuple[str, int]) -> str:
+    return code[0] + str(code[1])
+
+def print_hold_list(hold_list: List):
+    for i in range(len(hold_list)):
+        print(code_to_string(hold_list[i]), end = "")
+        if i < len(hold_list) - 1:
+            print(", ", end = "")
+        else:
+            print()
 
 if __name__ == "__main__":
     # Instructions:
-    # 1. Open emulator, download moonboard app, and log in. 
+    # 1. Open emulator, download moonboard app, and log in. (Pixel 4 API 30)
     # 2. Select desired moonboard layout, then sort all problems from oldest to newest.
     # 3. Change num_problems below to be the number of problems you want to scrape
     # 4. Select the oldest problem and run this script
 
-    num_problems = 57619
+    # TODO: make this a script with args
+    num_problems = 57624
+    out_data_path = "2016_data.json"
+    save_iter = 100
+
     device = connect_to_device()
+    data = []
+
+    for i in range(num_problems):
+        print(20 * "-" + f" Processing climb: {i + 1}/{num_problems} " + 20*"-")
+        img = imageio.v2.imread(io.BytesIO(device.screencap())) # shape = (2280, 1080, 4)
+
+        # get hold locations
+        start_holds = get_codes_for_color(img, START_COLOR)
+        middle_holds = get_codes_for_color(img, MIDDLE_COLOR)
+        finish_holds = get_codes_for_color(img, FINISH_COLOR)
+
+        # get climb basic info
+        name, grade, setter = get_basic_climb_info(img)
+        print(f"Name: {name}\nGrade: {grade}\nSetter: {setter}")
+        print(f"Start holds: ", end = "")
+        print_hold_list(start_holds)
+        print(f"Middle holds: ", end = "")
+        print_hold_list(middle_holds)
+        print(f"Finish holds: ", end = "")
+        print_hold_list(finish_holds)
 
 
-    for i in range(1):
-        img = imageio.imread(io.BytesIO(device.screencap())) # shape = (2280, 1080, 4)
 
-        start_codes = get_codes_for_color(img, START_COLOR)
-        middle_codes = get_codes_for_color(img, MIDDLE_COLOR)
-        finish_codes = get_codes_for_color(img, FINISH_COLOR)
-        print(start_codes)
-        print(middle_codes)
-        print(finish_codes)
+        # save data
+        data.append(
+            {
+                "setter": setter,
+                "grade": grade,
+                "name": name,
+                "start_holds": start_holds,
+                "middle_holds": middle_holds,
+                "finish_holds": finish_holds,
+            }
+        )
 
+        if i % save_iter == 0:
+            with open(out_data_path, "w") as f:
+                json.dump(data, f, indent=4)
 
-        #device.shell("input touchscreen swipe 700 1140 400 1140") # swipe to next 
+        device.shell("input touchscreen swipe 700 1140 400 1140") # swipe to next 
+
+    with open(out_data_path, "w") as f:
+        json.dump(data, f, indent = 4)
